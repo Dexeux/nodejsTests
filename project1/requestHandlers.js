bb13bc25f9c4dac716472272f04b4928974ef252
Chrisexn <cgaboury@live.com>
2016-06-12 19:43:40 -0400

    Postting data to server

--- project1/requestHandlers.js
+++ project1/requestHandlers.js
@@ -1,20 +1,36 @@
 var exec = require("child_process").exec;
 
-function start(response) {
+function start(response,postData) {
 	console.log("Request handler 'start' called");
-	
-	exec("ls -lah", function(error,stdout,stderr) {
+
+	var body = '<html>' + 
+		'<head>'+
+		'<meta http-equive = "Content-Type" content = "text/html; '+
+		'charset=UTF-8" />' +
+		'</head>' + 
+		'<body>' + 
+		'<form action = "/upload" method="post">' + 
+		'<textarea name="text" rows="20" cols="60"> </textarea>' +
+		'<input type ="submit" value = "Submit text" />' + 
+		'</form>' + 
+		'</body>' + 
+		'</html>';
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write(body);
+		response.end();	
+/*	exec("ls -lah", function(error,stdout,stderr) {
 		response.writeHead(200, {"Content-Type":"text/plain"});
 		response.write("Start menu");
 		response.write(stdout);
 		response.end();
 	});
+*/
 }
 
-function upload(response){
+function upload(response,postData){
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello upload");
+	response.write("Sent: "+ postData);
 	response.end();
 }
 
