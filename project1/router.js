1934aca7ef36c240eca97a608b743dce789f67a2
Chrisexn <cgaboury@live.com>
2016-06-12 19:25:44 -0400

    Added start page

--- project1/router.js
+++ project1/router.js
@@ -1,4 +1,12 @@
-function route(pathname){
+function route(handle,pathname,response){
 	console.log("Routing: " + pathname);
+	if(typeof handle[pathname] === 'function') {
+		handle[pathname](response);
+	}else{
+		console.log("Invalid handle url for: " + pathname);
+		response.writeHead(404, {"Content-Type":"text/plain"});
+		response.write("404 Url not found");
+		response.end();
+	}
 }
 exports.route = route;
