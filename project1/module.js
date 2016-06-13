9137dc550002227860fa8cfbbf431586fde2b322
Chrisexn <cgaboury@live.com>
2016-06-13 00:10:04 -0400

    end of tutorial was fun

--- project1/module.js
+++ project1/module.js
@@ -3,17 +3,17 @@ var url = require("url");
 
 function start(route,handle) {
 	function onRequest(request, response){
-		var postData = "";
+		//var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request received on "+pathname +" .");
-		request.setEncoding("utf8");
-		request.addListener("data",function(postDataChunk){
+		//request.setEncoding("utf8");
+		/*request.addListener("data",function(postDataChunk){
 			postData+= postDataChunk;
 			console.log("Received POST data chunk '" + postDataChunk + "'.");
-		});
-		request.addListener("end", function() {
-			route(handle,pathname,response,postData);
-		});	
+		});*/
+		//request.addListener("end", function() {
+			route(handle,pathname,response,request/*,postData*/);
+		//});	
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server started");
--- project1/requestHandlers.js
+++ project1/requestHandlers.js
@@ -1,6 +1,8 @@
-var exec = require("child_process").exec;
-var querystring = require("querystring");
-function start(response,postData) {
+var exec = require("child_process").exec,
+	querystring = require("querystring"),
+	fs = require("fs"),
+	formidable = require("formidable");
+function start(response/*,postData*/) {
 	console.log("Request handler 'start' called");
 
 	var body = '<html>' + 
@@ -9,9 +11,10 @@ function start(response,postData) {
 		'charset=UTF-8" />' +
 		'</head>' + 
 		'<body>' + 
-		'<form action = "/upload" method="post">' + 
-		'<textarea name="text" rows="20" cols="60"> </textarea>' +
-		'<input type ="submit" value = "Submit text" />' + 
+		'<form action = "/upload" enctype="multipart/form-data" ' +
+		'method="post">' + 
+		'<input type = "file" name = "upload"i multiple = "multiple">' + 	
+		'<input type ="submit" value = "Upload a file" />' + 
 		'</form>' + 
 		'</body>' + 
 		'</html>';
@@ -27,13 +30,49 @@ function start(response,postData) {
 */
 }
 
-function upload(response,postData){
+function upload(response,request/*,postData*/){
 	console.log("Request handler 'upload' was called");
+	
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function (error, fields , files){
+		console.log("parsing done");
+		
+		fs.rename(files.upload.path, "/tmp/test.png",function(err){
+			if(err){
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src = '/show' />");
+		response.end();
+	});
+}
+	
+	/*
 	response.writeHead(200,{"Content-Type":"text/plain"});
 	response.write("Sent: "+ querystring.parse(postData).text);
 	response.end();
+	*/
+
+function show(response/*, postData*/){
+	console.log("Request handler ' show' was called.");
+	fs.readFile("/tmp/test.png","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-Type":"text/plain"});
+			response.write(err + "\n");
+			response.end();
+		}else{
+			response.writeHead(200,{"Content-Type":"image/png"});
+			response.write(file,"binary");
+			response.end();
+		}
+	});
 }
+
 
 exports.start = start;
 exports.upload = upload;
-
+exports.show = show;
